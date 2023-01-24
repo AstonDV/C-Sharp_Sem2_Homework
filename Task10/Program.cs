cfd66bcd4da26f4e712b1b8f6a62e2a39e736fb2
AstonDV <astondv.kms@gmail.com>
2023-01-25 01:11:11 +1000

    End task 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+int a2 = num % 100 / 10;
+if (num < 100 || num > 999)
+{
+    Console.WriteLine("Введенное число не является трехзначным, попробуйте снова");
+}
+else
+{
+    Console.WriteLine(a2);
+}
