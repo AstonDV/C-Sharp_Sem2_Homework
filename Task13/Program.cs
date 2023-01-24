7b102ff344f4a652ca711dbb87f681afa672fb8f
AstonDV <astondv.kms@gmail.com>
2023-01-25 01:34:20 +1000

    End task 13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. (Цифры считать справа налево).
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int searchNum = num / 100 % 10;
+if (searchNum < 1)
+{
+    Console.WriteLine("Третьей цифры нет!!!");
+}
+else
+{
+    Console.WriteLine(searchNum);
+}
